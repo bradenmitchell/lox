bf2691194781f4358e2b45ade29ae2d5239a781d
Braden Mitchell <32005299+bradenmitchell@users.noreply.github.com>
2024-03-06 12:34:19 +1030

    feat: Chapter 22 "Local Variables"

--- src/vm.c
+++ src/vm.c
@@ -108,10 +108,20 @@ static InterpretResult run(void) {
         push(constant);
         break;
       }
-      case OP_NIL:        push(NIL_VAL); break;
-      case OP_TRUE:       push(BOOL_VAL(true)); break;
-      case OP_FALSE:      push(BOOL_VAL(false)); break;
-      case OP_POP:        pop(); break;
+      case OP_NIL:       push(NIL_VAL); break;
+      case OP_TRUE:      push(BOOL_VAL(true)); break;
+      case OP_FALSE:     push(BOOL_VAL(false)); break;
+      case OP_POP:       pop(); break;
+      case OP_GET_LOCAL: {
+        uint8_t slot = READ_BYTE();
+        push(vm.stack[slot]);
+        break;
+      }
+      case OP_SET_LOCAL: {
+        uint8_t slot   = READ_BYTE();
+        vm.stack[slot] = peek(0);
+        break;
+      }
       case OP_GET_GLOBAL: {
         ObjString* name = READ_STRING();
         Value value;
